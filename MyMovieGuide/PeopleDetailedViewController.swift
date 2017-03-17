9cbcd446d59edfdc3eabdbdc44801f22ca577f60
JD <asentwo@hotmail.com>
2017-03-17 16:14:19 -0500

    people redesign storyboard

---
+++ MyMovieGuide/PeopleDetailedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PeopleDetailedViewController.swift
+//  MyMovieGuide
+//
+//  Created by Justin Doo on 3/17/17.
+//  Copyright © 2017 JustinDoo. All rights reserved.
+//
+
+import Foundation
